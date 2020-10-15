5dabecfae234e619df9d9aa0e226a96cd494d5eb
unsivil <unsivilaudio@gmail.com>
2020-10-15 10:11:29 -0600

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from '../assets/stylesheets/app.module.css';
+
+const app = props => {
+    return (
+        <div className={classes.App}>
+            <h1>The App component</h1>
+        </div>
+    );
+};
+
+export default app;
